734347eb2b959164147c174176963167ab2098ef
Bartu10 <jbg10102002@gmail.com>
2022-12-07 09:29:28 +0100

    x+11

--- dist/index.bda4afae.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".cara").forEach(function(elemento) {
-    elemento.addEventListener("click", function() {
-        this.classList.toggle("detras");
-    });
-});
-
-//# sourceMappingURL=index.bda4afae.js.map
--- src/assets/js/index.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".cara").forEach(function(elemento) {
-    elemento.addEventListener("click", function() {
-      this.classList.toggle("detras");
-    });
-  });
